f05416d6642b030e0ed3753e00a3280393518cd8
AideTechBot <mandio0302@gmail.com>
2023-09-14 23:33:46 -0400

    refactor(redesign): :fire: clean up markup and styles for header

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { homePageHandler } from "./routes/home-page/home-page.handler";
 const app = new Hono();
 
 // Serve static files from the `public` folder
-app.use("/styles/*", serveStatic({ root: "./public" }));
-app.use("/public/*", serveStatic({ root: "./public" }));
+// app.use("/styles/*", serveStatic({ root: "./public" }));
+app.use("*", serveStatic({ root: "./public" }));
 app.use("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }));
 
 // Log all requests
