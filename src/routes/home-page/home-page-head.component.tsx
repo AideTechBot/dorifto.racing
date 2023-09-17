60cba0b74ce2d96b720bc6b9844f2823dfa7c499
AideTechBot <mandio0302@gmail.com>
2023-09-17 14:03:27 -0400

    refactor(redesign): :sparkles: complete front-end
    Completed front-end by cleaning up tags and styling. Revamped it a bit to appease lighthouse.

--- src/routes/home-page/home-page-head.component.tsx
+++ src/routes/home-page/home-page-head.component.tsx
@@ -1,14 +1,16 @@
-export function HomePageHead(props: { title: string }) {
-  const { title } = props;
+export function HomePageHead(props: { title: string; description: string }) {
+  const { title, description } = props;
   return (
     <head>
+      <title>{title}</title>
+
+      <meta name="description" content={description} />
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <title>{title}</title>
+      <script src="https://unpkg.com/htmx.org@1.9.5" />
 
       <link href="styles/style.css" rel="stylesheet" />
-
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
