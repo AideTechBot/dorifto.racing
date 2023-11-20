1eeded106fbb108fe996df76bcada70074635479
AideTechBot <mandio0302@gmail.com>
2023-11-20 17:25:38 -0500

    feat(redesign): :chart_with_upwards_trend: add google analytics

--- src/routes/home-page/home-page-head.component.tsx
+++ src/routes/home-page/home-page-head.component.tsx
@@ -1,3 +1,5 @@
+import { html } from "hono/html";
+
 export function HomePageHead(props: { title: string; description: string }) {
   const { title, description } = props;
   return (
@@ -9,6 +11,19 @@ export function HomePageHead(props: { title: string; description: string }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <script src="https://unpkg.com/htmx.org@1.9.5" />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NRF6E5V374"
+      />
+      {html`<script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-NRF6E5V374");
+      </script>`}
 
       <link href="styles/style.css" rel="stylesheet" />
       <link
